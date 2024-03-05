48b75aa67234f3b4f66efc7f2fa271b9633d913d
Khadijaaog <158587434+Khadijaaog@users.noreply.github.com>
2024-03-05 17:41:53 +0100

    barra

--- app/app.py
+++ app/app.py
@@ -8,6 +8,32 @@ def index():
     #return "Hola Mundo"
     return render_template('index.html')
 
+@app.route('/index.html')
+def index():
+    #return "Hola Mundo"
+    return render_template('index.html')
+
+@app.route('/pg_dona2.html')
+def pg_dona2():
+    #return la pagina de inciar session
+    
+    return render_template('pg_dona2.html')
+
+@app.route('/mapa.html')
+def mapa():
+    #return la pagina de inciar session
+    
+    return render_template('mapa.html')
+
+@app.route('/contacto.html')
+def contacto():
+    #return la pagina de inciar session
+    
+    return render_template('contacto.html')
+
+
+
+
 if __name__ == '__main__':
     #lo que hay () sireve para poder editar sin reiniciar el server, modo depuracion
     
