374bb10003a7cba20618b73b6bdf9a6076334345
sercho92 <162343139+sercho92@users.noreply.github.com>
2024-03-22 15:31:25 +0100

    nuevo

--- app/app.py
+++ app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request, redirect , url_for
 import os
 import database as db 
 
@@ -10,7 +10,7 @@ app=Flask(__name__, template_folder = template_dir)
 @app.route('/')
 def home():
     cursor = db.database.cursor()
-    cursor.execute("SELECT * FROM user")
+    cursor.execute("SELECT * FROM users")
     myresult= cursor.fetchall()
     #convertir los datos a diccionario
     insertObject = []
@@ -18,9 +18,44 @@ def home():
     for record in myresult:
         insertObject.append(dict(zip(columnNames, record)))
     cursor.close()
-    return render_template('index.html')
+    return render_template('index.html', data=insertObject)
+#Ruta para guardar usuarios a la base de datos
+@app.route('/user', methods=['POST'])
+def addUser():
+    username = request.form['username']
+    name = request.form['name']
+    password = request.form['password']
 
+    if username and name and password:
+        cursor = db.database.cursor()
+        sql = "INSERT INTO users (username, name, password) VALUES (%s, %s, %s)"
+        data=(username, name, password)
+        cursor.execute(sql, data)
+        db.database.commit()
+    return redirect(url_for('home'))
+#consulta eliminar un user
+@app.route('/delete/<string:id>')
+def delete(id):
+      cursor = db.database.cursor()
+      sql = "DELETE FROM users WHERE id=%s)"
+      data=(id,)
+      cursor.execute(sql, data)
+      db.database.commit()
+      return redirect(url_for('home'))
 
+@app.route('/edit/<string:id>',methods=['POST'])
+def edit(id):
+      username = request.form['username']
+      name = request.form['name']
+      password = request.form['password']
+#para modificar los datos
+      if username and name and password:
+        cursor = db.database.cursor()
+        sql = "UPDATE  users SET username = %s, name= %s, password=%s WHERE id =%s"
+        data=(username, name, password)
+        cursor.execute(sql, data)
+        db.database.commit()
+      return redirect(url_for('home'))
 
 @app.route('/')
 def index():
